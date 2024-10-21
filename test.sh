11d5820ead6d77da5d7cb47d7c464d2090ef0bf2
Tanapat Sa-nguantud <6510615120@cn330>
2024-10-21 14:02:16 +0700

    test.sh

--- test.sh
+++ test.sh
@@ -3,3 +3,4 @@ python -c "from test_prime import test_prime; test_prime(3, True)"
 python -c "from test_prime import test_prime; test_prime(5, True)"
 python -c "from test_prime import test_prime; test_prime(10, False)"
 python -c "from test_prime import test_prime; test_prime(25, False)"
+echo hi
